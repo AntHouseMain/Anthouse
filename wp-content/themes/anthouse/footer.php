7ff28ac9e6dce0cf2214f81f5554df80c69407b9
anhousecode <bsmgroup69@gmail.com>
2018-02-28 16:50:18 +0200

    Fixed social icons in footer

--- wp-content/themes/anthouse/footer.php
+++ wp-content/themes/anthouse/footer.php
@@ -11,7 +11,6 @@
 
 ?>
 </main>
-
 <footer>
 	<div class="uk-container">
 		<div uk-grid>
@@ -22,44 +21,46 @@
 						<?php while( have_rows('footer_phone',2) ): the_row(); ?>
 							<li>
 								<a  href="tel:<?php echo get_sub_field('footer_phone_item'); ?>">
-									<?php echo get_sub_field('footer_phone_item',$post->ID,2); ?>
+									<?php echo get_sub_field('footer_phone_item',2); ?>
 								</a>
 							</li>
 						<?php endwhile; ?>
 						<li>
 							<a href="mailto:<?php echo get_sub_field('footer_mail'); ?>">
-								<?php echo get_field('footer_mail',$post->ID); ?>
+								<?php echo get_field('footer_mail',2);
+
+								 ?>
 							</a>
 						</li>
 						<li>
 							<a href="<?php echo get_sub_field('footer_skype'); ?>">
-								<?php echo get_field('footer_skype',$post->ID); ?>
+								<?php echo get_field('footer_skype',2); ?>
 							</a>
 						</li>
 						<li>
 							<a href="<?php echo get_field('url_footer_location'); ?>">
-								<?php echo get_field('footer_location',$post->ID); ?>
+								<?php echo get_field('footer_location',2); ?>
 							</a>
 						</li>
 					</ul>
 				<?php endif; ?>
 			</div>
 			<div class="uk-width-2-3@s uk-width-2-3@m">
-				<h2><?php echo get_field('footer_title_2',$post->ID) ?></h2>
-				<?php echo get_field('footer_description',$post->ID); ?>
+				<h2><?php echo get_field('footer_title_2',2); ?></h2>
+				<?php echo get_field('footer_description',2); ?>
 			</div>
 		</div>
 
 		<div uk-grid>
 			<div class="uk-social uk-width-1-1 uk-width-1-3@s uk-width-1-3@m uk-flex uk-flex-row uk-flex-wrap ">
-				<a href="<?php echo get_field('footer_facebook',$post->ID); ?>"></a>
-				<!-- <a href="skype:<?php echo get_field('footer_skype',$post->ID); ?>"></a> -->
-				<a href="<?php echo get_field('footer_linkedin',$post->ID); ?>"></a>
-				<a href="<?php echo get_field('footer_google_plus',$post->ID); ?>"></a>
-				<a href="<?php echo get_field('footer_upwork',$post->ID); ?>"></a>
+				<a href="<?php echo get_field('footer_facebook',2); ?>"></a>
+				<!-- <a href="skype:<?php echo get_field('footer_skype',2); ?>"></a> -->
+				<a href="<?php echo get_field('footer_linkedin',2); ?>"></a>
+				<a href="<?php echo get_field('footer_google_plus',2); ?>"></a>
+				<a href="<?php echo get_field('footer_upwork',2); ?>"></a>
 			</div>
 			<div class="uk-width-1-1 uk-width-2-3@s uk-width-2-3@m uk-flex uk-flex-right@s  uk-flex-left uk-flex-wrap uk-flex-middle">
-				<h3><?php echo get_field('footer_copyright',$post->ID) ?></h3>
+				<h3><?php echo get_field('footer_copyright',2) ?></h3>
 			</div>
 		</div>
 	</div>
