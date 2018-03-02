e96ddb5c5235b9035049e2accec8b12afcb39882
anhousecode <bsmgroup69@gmail.com>
2018-03-02 15:34:43 +0200

    Changed phones on contacts page

--- wp-content/themes/anthouse/template-parts/template-contacts.php
+++ wp-content/themes/anthouse/template-parts/template-contacts.php
@@ -11,26 +11,26 @@ get_header(); ?>
 		<div class="info-title" >
 			<h2 class="uk-text-uppercase" uk-scrollspy="cls: uk-animation-scale-up; repeat: true">CONTACT US </h2>
 			<hr uk-scrollspy="cls: uk-animation-fade; repeat: true">
-			<p>Ukraine, Kharkiv  |  itant.house@gmail.com  |   (067) 487 35 14  |  (067) 536 78 95
+			<p>Ukraine, Kharkiv  |  itant.house@gmail.com  |   (063) 782 06 36  |  (096) 176 76 98
 			</p>
 		</div>
 	</div>
 </div>
 
-<!--== Just-ask ==-->	
+<!--== Just-ask ==-->
 <section class="uk-section">
 	<div class="uk-container">
-		<div uk-grid> 
+		<div uk-grid>
 			<div class="uk-width-1-1 uk-text-center">
 				<h2 class="uk-heading-line uk-text-uppercase">
 					<span>JUST ASK US ANYTHING</span>
-				</h2>				
+				</h2>
 				<b>The form below can help us help you better</b>
 			</div>
 		</div>
 	</div>
 </section>
-<!--== /Just-ask ==-->	
+<!--== /Just-ask ==-->
 <!--== Form ==-->
 <section class="uk-contact-form-section">
 	<div class="uk-container">
